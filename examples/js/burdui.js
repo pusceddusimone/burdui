16b71a28c5b1fd4ae71510bfe84b7b99d7a4ae44
pusceddusimone <92328763+pusceddusimone@users.noreply.github.com>
2023-07-12 19:47:34 +0200

    Initial commit

--- examples/js/burdui.js
+++ examples/js/burdui.js
@@ -1724,6 +1724,90 @@
 
 	window.customElements.define('bui-text-field', TextFieldElement);
 
+	function Window(bounds){
+	    View.call(this);
+	    this.bounds = bounds || new Bounds();
+	    this.border = new Border();
+	    this.background = new Background();
+	}
+
+
+	Window.prototype = Object.assign( Object.create( View.prototype ), {
+
+	    constructor: Window,
+
+	    setBounds: function(bounds){
+	        this.bounds = bounds;
+	        this.border.setBounds(new Bounds(0,0, this.bounds.w, this.bounds.h));
+	        this.background.setBounds(new Bounds(
+	            this.border.lineWidth/2,
+	            this.border.lineWidth/2,
+	            this.bounds.w - this.border.lineWidth,
+	            this.bounds.h - this.border.lineWidth));
+	        this.updateBounds();
+	        return this;
+	    },
+
+	    getBounds : function(){
+	        return this.bounds;
+	    },
+
+
+	    addChild: function(child){
+	        View.prototype.addChild.call(this, child);
+
+	        // not optimized, we can speed-up setting the bounds of the last child.
+	        this.updateBounds();
+
+	        return this;
+	    },
+
+	    updateBounds: function(){
+	        let next = 0;
+	        for(let i in this.children) {
+	            let c = this.children[i];
+	            switch(this.style){
+	                case "vertical":
+	                    c.setBounds(new Bounds(0, next, this.bounds.w, c.bounds.h));
+	                    next += c.bounds.h + this.padding;
+	                    break;
+
+	                case "horizontal":
+	                    c.setBounds(new Bounds(next, 0, c.bounds.w, this.bounds.h));
+	                    next += c.bounds.w + this.padding;
+	                    break;
+	            }
+	        }
+	    },
+
+
+	    /**
+	     * Paints the window, the window just acts as a container for the windowgroup
+	     * @param g the canvas
+	     * @param r the root
+	     */
+	    paint: function(g, r){
+	        r = r || this.bounds;
+	        this.border.paint(g, r);
+	        this.paintChildren(g, r);
+	    },
+	});
+
+	class WindowElement extends ViewElement {
+	    constructor() {
+	        super();
+	        this.buiView = new Window();
+	    }
+	    connectedCallback() {
+	        super.connectedCallback();
+	    }
+
+
+
+	}
+
+	window.customElements.define('bui-window', WindowElement);
+
 	exports.App = App;
 	exports.Background = Background;
 	exports.Border = Border;
@@ -1742,6 +1826,7 @@
 	exports.TextFieldElement = TextFieldElement;
 	exports.View = View;
 	exports.ViewElement = ViewElement;
+	exports.WindowElement = WindowElement;
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 
---
+++ src/declarative/windowElement.js
@@ -0,0 +1,19 @@
+import {ViewElement} from "./viewElement";
+import {Window} from "../views/window";
+
+class WindowElement extends ViewElement {
+    constructor() {
+        super();
+        this.buiView = new Window();
+    }
+    connectedCallback() {
+        super.connectedCallback();
+    }
+
+
+
+}
+
+window.customElements.define('bui-window', WindowElement);
+
+export {WindowElement};
